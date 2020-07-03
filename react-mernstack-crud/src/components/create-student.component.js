8b2be47eecbb80f1fc80e5b87dbc59f14ed1e067
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-07-03 12:54:04 +0700

    Add basic functionalitiies

---
+++ react-mernstack-crud/src/components/create-student.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Buttom from 'react-bootstrap/Button';
+export default class CreateStudent extends Component {
+	render() {
+		return (
+			<div>
+			  <p>React Create Student Component!</p>
+			</div>
+		);
+	}
+}
